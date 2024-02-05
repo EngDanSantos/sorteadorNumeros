703e7a9ddb93b184789b89f4b244cda192995afd
Dan Santos <110039343+EngDanSantos@users.noreply.github.com>
2024-02-05 17:01:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ function sortear(){
     
     let sorteados = [];
     let numero;
-    
+
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+      }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        return;
+     }
+
     for (let i = 0; i <quantidade; i++){
         numero = numeroAleatorio(de,ate);
         while(sorteados.includes(numero)){
